9a3bf21cd0a67f6f951b243f7f30d6fd568f0540
ivanezzio <ivanvozny@gmail.com>
2020-09-06 00:25:21 +0300

    го

---
+++ src/test/java/tests/issuetests.java
@@ -0,0 +1,25 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class issuetests {
+
+
+    @Test
+    public void createNewIssue() {
+        Configuration. holdBrowserOpen = true;
+        open("https://github.com");
+        //$(".header-search-input").click();
+        //$(".header-search-input").sendKeys("");
+
+
+
+    }
+
+
+}
+
