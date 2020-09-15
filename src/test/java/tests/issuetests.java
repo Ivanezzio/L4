8d218db62acbe4cc0d18f35a2d5968046cf8b070
ivanezzio <ivanvozny@gmail.com>
2020-09-15 21:57:55 +0300

    го

--- src/test/java/tests/issuetests.java
+++ src/test/java/tests/issuetests.java
@@ -2,7 +2,9 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
+import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -13,8 +15,12 @@ public class issuetests {
     public void createNewIssue() {
         Configuration. holdBrowserOpen = true;
         open("https://github.com");
-        //$(".header-search-input").click();
-        //$(".header-search-input").     sendKeys("");
+        $(".header-search-input").click();
+        $(".header-search-input").sendKeys("ivanezzio/L4");
+        $(".header-search-input").submit();
+        $(By.linkText("Ivanezzio/L4")).click();
+        $(withText("Issues")).click();
+
 
 
 
